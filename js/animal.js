bed887957f5f47a0f4918812795d7f15a955e706
Dantesakt1 <dvntecastillo@gmail.com>
2025-09-16 10:26:01 -0300

    Se arreglan las clases y se agrega la clase productoAnimal

--- js/animal.js
+++ js/animal.js
@@ -3,11 +3,13 @@ class Animal{
     id;
     nombre;
     precio;
+    imagen;
 
-    constructor(id,nombre,precio){
+    constructor(id,nombre,precio,imagen){
         this.id =id;
         this.nombre=nombre;
         this.precio = precio;
+        this.imagen=imagen;
     }
     
 }
---
+++ js/productoAnimal.js
@@ -0,0 +1,19 @@
+
+class ProductoAnimal {
+    id;
+    nombre;
+    precio;
+    cantidad;
+    total;
+    imagen;
+
+
+    constructor(id, nombre, precio, imagen) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.cantidad = 1;
+        this.total = precio;
+        this.imagen = imagen;
+    }
+}
