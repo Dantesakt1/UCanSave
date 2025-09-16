2dfe2d9fdf89ac6dccd02806400210ca5999f0e6
Dantesakt1 <dvntecastillo@gmail.com>
2025-09-16 09:10:34 -0300

    Se arreglan cosas del menu.html y se agrega la clase animal

---
+++ js/animal.js
@@ -0,0 +1,13 @@
+class Animal{
+
+    id;
+    nombre;
+    precio;
+
+    constructor(id,nombre,precio){
+        this.id =id;
+        this.nombre=nombre;
+        this.precio = precio;
+    }
+    
+}
